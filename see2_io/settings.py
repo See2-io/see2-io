ecf2e945a1d7ebba149edc09aa14fe5502ca239a
Dave D <40235800+See2-io@users.noreply.github.com>
2019-01-02 21:51:03 +0000

    Add new app, aia - Augmented Intelligence Academy.

--- see2_io/settings.py
+++ see2_io/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'aia.apps.AiaConfig',
     'users.apps.UsersConfig',
     'sense.apps.SenseConfig',
     'communities.apps.CommunitiesConfig',
